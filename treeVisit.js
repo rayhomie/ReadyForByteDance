9312e39eba2ca593184b4eb0e36ac34159752143
rayhomie <1572801584@qq.com>
2021-09-21 15:13:36 +0800

    后序遍历、层级遍历

--- treeVisit.js
+++ treeVisit.js
@@ -48,6 +48,7 @@ function inOrder(root) {
   return res;
 }
 
+//将左叶子全部压入栈
 function inOrderStack(root) {
   const res = [];
   if (!root) return res;
@@ -71,4 +72,73 @@ function inOrderStack(root) {
   return res;
 }
 
-function postOrder(root) {}
+function postOrder(root) {
+  const res = [];
+  if (!root) return res;
+  function handle(node) {
+    if (node.left) {
+      handle(node.left);
+    }
+    if (node.right) {
+      handle(node.right);
+    }
+    res.push(node.val);
+  }
+  handle(root);
+  return res;
+}
+
+//将左叶子全部压入栈
+function postOrderStack(root) {
+  const res = [];
+  if (!root) return res;
+  let stack = [];
+  let temp = root;
+  while (temp) {
+    //将左叶子全部压入栈
+    stack.push(temp);
+    temp = temp.left;
+  }
+  let prev = null; //用于记录上一次的弹出节点
+  while (stack.length > 0) {
+    const top = stack[stack.length - 1];
+    if (!top.right || (top.right && prev === top.right)) {
+      /*如果当前节点没有右叶子 
+        或者
+        如果上一次弹出的节点 和 当前的右叶子地址相同，则记录结果（最先进入的右叶子也需要结束）*/
+      prev = stack.pop();
+      res.push(top.val);
+    } else {
+      //否则将所有左叶子压入
+      let temp2 = top.right;
+      while (temp2 !== null) {
+        stack.push(temp2);
+        temp2 = temp2.left;
+      }
+    }
+  }
+  return res;
+}
+
+function levelOrder(root) {
+  const res = [];
+  if (!root) return res;
+  const queue = [];
+  queue.push(root);
+  while (queue.length > 0) {
+    const curRes = [];
+    const n = queue.length; //**queue的长度在变化，需要进入下一层时写死
+    for (let i = 0; i < n; i++) {
+      const cur = queue.shift();
+      curRes.push(cur.val);
+      if (cur.left) {
+        queue.push(cur.left);
+      }
+      if (cur.right) {
+        queue.push(cur.right);
+      }
+    }
+    res.push(curRes);
+  }
+  return res;
+}
