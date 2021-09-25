c0eb7dfee063deab2340f915c3269692b1e94a60
rayhomie <1572801584@qq.com>
2021-09-25 11:25:06 +0800

    函数柯里化

--- curry.js
+++ curry.js
@@ -9,7 +9,8 @@ fn(1)(2, 3); // 6
 fn(1)(2)(3); // 6
 */
 
-function curry(callback) {
+//使用调用静态方法的形式来结束
+function curry1(callback) {
   const params = [];
   return function handle(...args) {
     params.push(...args);
@@ -19,9 +20,26 @@ function curry(callback) {
     return handle;
   };
 }
+
+//根据传入传入参数来结束
+function curry2(callback) {
+  const params = [];
+  return function handle(...args) {
+    params.push(...args);
+    if (callback.length === params.length) {
+      //参数收集够了则调用函数
+      callback(...params);
+    }
+    return handle;
+  };
+}
+
+//test
 function sum(a, b, c) {
   console.log(a + b + c);
 }
-const fn = curry(sum);
-fn(1, 2, 3).toString(); // 6
-// fn(1, 2)(3)(4, 5).toString(); // 6
+const fn1 = curry1(sum);
+fn1(1, 2)(3)(4, 5).toString(); // 6
+
+const fn2 = curry2(sum);
+fn2(1, 2)(3)(4, 5); // 6
